780c32aee78faa57f43060d647b1c8b43e1e95d7
PatrickRobotique <80887734+PatrickRobotique@users.noreply.github.com>
2021-05-14 17:50:33 +0200

    Update main.c
    Main plus clean/nveau thread sensors

--- miniprojet/test/main.c
+++ miniprojet/test/main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "ch.h"
 #include "hal.h"
 #include "memory_protection.h"
 #include <usbcfg.h>
 #include <main.h>
-#include <chprintf.h>
 #include <motors.h>
 #include <audio/microphone.h>
 #include <sensors/proximity.h>
 #include <audio_processing.h>
 #include <direction.h>
-#include <fft.h>
-#include <arm_math.h>
+
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
@@ -21,62 +20,42 @@ CONDVAR_DECL(bus_condvar);
 parameter_namespace_t parameter_root;
 
 
-static void serial_start(void)
-{
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
-
-	sdStart(&SD3, &ser_cfg); // UART3.
-}
-
-
-static THD_WORKING_AREA(waThdSensor, 2048);
-static THD_FUNCTION(ThdSensor, arg) {
-
-    chRegSetThreadName(__FUNCTION__);
-    (void)arg;
-
-    while(1){
-    	movebitch();
-        /*
-        *   1st case :  pause the thread during 100ms
-        */
-         chThdSleepMilliseconds(100);
-    }
-
-}
-
-
 int main(void)
 {
-    //Ici nous avons fait toutes les initialisations nécessaire pour le programme
+
     halInit();
     chSysInit();
     mpu_init();
-    //start motors
-    motors_init();
+
     /** Inits the Inter Process Communication bus. */
     messagebus_init(&bus, &bus_lock, &bus_condvar);
     parameter_namespace_declare(&parameter_root, NULL, NULL);
-    //starts the serial communication
-    serial_start();
+
+    //starting motors
+    motors_init();
     //starts the USB communication
     usb_start();
-    //start microphone
-    mic_start(&processAudioData);
+
     //start prox sensor
     proximity_start();
+    //proximity.c library, calibration of all 8 IR sensors
     calibrate_ir();
-    chThdCreateStatic(waThdSensor, sizeof(waThdSensor), NORMALPRIO, ThdSensor, NULL);
+    //start microphone
+    mic_start(&processAudioData);
+    //Starts the thread for the sensors
+    thd_sensor_start();
     /* Infinite loop. */
     while (1){
     }
 }
+
+
+
+
+
 #define STACK_CHK_GUARD 0xe2dee396
+
+
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
 
