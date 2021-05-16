3e6dc5317da75a762f6d8a986c81b580196e90bf
PatrickRobotique <80887734+PatrickRobotique@users.noreply.github.com>
2021-05-16 21:43:43 +0200

    Update direction.c
    static int

--- miniprojet/test/direction.c
+++ miniprojet/test/direction.c
@@ -1,6 +1,6 @@
-/*Cette partie du programme détermine la vitesse des moteurs.
+/*Cette partie du programme dÃ©termine la vitesse des moteurs.
 Il y a aussi la gestion des capteurs infrarouges ainsi que
-l'algorithme de Braitenberg, inspiré d'un exercice de Distributed Intelligent System
+l'algorithme de Braitenberg, inspirÃ© d'un exercice de Distributed Intelligent System
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +14,8 @@ l'algorithme de Braitenberg, inspiré d'un exercice de Distributed Intelligent Sy
 #include <arm_math.h>
 
 // Poids des capteurs IR
-int weightleft[8] = {-5, -5, -5, 0, 0, 5, 5, 5};
-int weightright[8] = {5, 5, 5, 0, 0, -5, -5, -5};
+static int weightleft[8] = {-5, -5, -5, 0, 0, 5, 5, 5};
+static int weightright[8] = {5, 5, 5, 0, 0, -5, -5, -5};
 
 static int Speed[4] = {0,0,0,0};
 #define SPEEDLAUDIO 0
@@ -47,16 +47,16 @@ void sensor_values(void){
     for (i=0;i<NUM_CAPTEUR;i++){
     	sensorMean[i]=0;
     }
-    //On fait une moyenne des mesures des capteurs IR pour plus de précision
+    //On fait une moyenne des mesures des capteurs IR pour plus de prÃ©cision
     for (n=0;n<numberOfSamples;n++){
     	//Mesures capteurs IR
     	for (i = 0; i < NUM_CAPTEUR; i++) {
     		sensor[i] = get_calibrated_prox(i);
-    		//linéarisation des mesures
+    		//linÃ©arisation des mesures
     		sensorMean[i]+=12.1514*log((double)sensor[i]+1)/(double)numberOfSamples;
     	}
     }
-	// conditions des capteurs IR pour savoir si ils contrôlent les moteurs
+	// conditions des capteurs IR pour savoir si ils contrÃ´lent les moteurs
 	if(sensorMean[AVT_CNTRE_DT]>DIST_OBJET || sensorMean[AVT_CNTRE_GC]>DIST_OBJET||
 		sensorMean[AVT_DT]> DIST_OBJET || sensorMean[AVT_GC]>DIST_OBJET ||
 		sensorMean[DROITE]>DIST_OBJET  || sensorMean[GAUCHE]>DIST_OBJET){
